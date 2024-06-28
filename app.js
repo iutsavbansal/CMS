7a0ad14e99f23bbffd3b2bf9355248131170684e
Utsav Bansal <utsavbansal75@gmail.com>
2024-06-29 00:03:00 +0530

    admin

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
-
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
 const connectDB = require('./server/config/db');
 
 
@@ -14,6 +17,18 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+
+}));
 
 
 app.use(express.static('public'));
@@ -23,7 +38,8 @@ app.use(expressLayout);
 app.set('layout','./layouts/main');
 app.set('view engine','ejs');
 
-app.use('/',require('./server/routes/main'))
+app.use('/',require('./server/routes/main'));
+app.use('/',require('./server/routes/admin'));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
