06e1bc2780f2a60e27a01ffcf4f22ca6a09a6dc2
Utsav Bansal <utsavbansal75@gmail.com>
2024-06-28 17:37:55 +0530

    searchbar

--- server/routes/main.js
+++ server/routes/main.js
@@ -75,6 +75,35 @@ router.get('/post/:id', async (req,res) => {
     }
 });
 
+/**
+ * Get /
+ * Post - searchTerm
+ */
+
+router.post('/search', async (req,res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Simple Blog created with NodeJs, Express & MongoDb."
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+        const data = await Post.find({
+            $or: [
+                {title: { $regex: new RegExp(searchNoSpecialChar, 'i')}},
+                {body: { $regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        });
+        res.render("search",{
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 router.get('/about', (req,res) => {
     res.render('about');
